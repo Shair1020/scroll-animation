bc762d41892f12b3949a484040715f48523b2383
shair1020 <ali99shair@gmail.com>
2021-05-27 23:51:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const checkBoxes = () => {
+  const triggerBottm = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottm) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+};
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
